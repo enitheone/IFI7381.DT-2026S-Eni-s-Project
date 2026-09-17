16a329dd9dae1708f1f0b7de9570d84411d4304a
Eni <erik.nikiforov@gmail.com>
2026-09-17 16:37:58 +0300

    Verifying fun
    * in calcFun, changing original values to assure that the variable adjustments make the initial values pointless

--- Problem Sets/#3/Scripts/Player.gd
+++ Problem Sets/#3/Scripts/Player.gd
@@ -119,12 +119,12 @@ func calc_2(a: int = 0, b: int = 0):
 	return new_a
 	
 func calcFun():
-	var a: int = 5
+	var a: int = 579
 	print("First Fun a starts: ", a)
 	a = calc_1(a)
 	print("First Fun a ends: ", a)
-	a = 3
-	var b: int = 2
+	a = 3135
+	var b: int = 1100
 	print("Second Fun a starts: ", a)
 	a = calc_2(a,b)
 	print("Second Fun a ends: ", a)
