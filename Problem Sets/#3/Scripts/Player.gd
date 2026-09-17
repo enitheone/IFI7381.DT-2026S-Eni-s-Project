2e08b94da3b67cddd4a3dbaa004050e6bb6a7e7a
Eni <erik.nikiforov@gmail.com>
2026-09-17 15:07:44 +0300

    Started with Part 2
    Made Part 2 with some trials and errors.
    Biggest error to deal with was
    "Used tab character for indentation instead of space as used before in the file."
    => After couple of tries, Godot accepted my Enter presses without complaining about tabs and spaces.

--- Problem Sets/#3/Scripts/Player.gd
+++ Problem Sets/#3/Scripts/Player.gd
@@ -62,6 +62,19 @@ const max_health: int = 100 #— an integer, also 100 (make this one a constant)
 var speed: int = 50  #— an integer, starting at 50
 var player_name: String = "Aye" #— a string, set to whatever you like
 
+#Part 2
+func inform_stats():
+	print("Player: ", player_name," || Health/Max Health: ",health,"/",max_health)
+
+func take_damage(amount: int = 0):
+	var delta: = health - amount
+	if delta < 0:
+		delta = 0
+	health = delta
+	print("Took damage: ",delta)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	inform_stats()
+	take_damage(15)
+	inform_stats()
